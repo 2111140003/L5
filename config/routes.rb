51f5dd9cbd0bf6bf4161e0352a2d9b40cc6bce0a
Arai Taichi <ubuntu@ip-172-31-18-145.ec2.internal>
2023-10-31 14:47:52 +0000

    いいね追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'top/logout'
   resources :users 
   resources :tweets
+  resources :likes, only: [:create, :destroy]
+
   root 'tweets#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
